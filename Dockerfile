b1a5f9b071a9625effd73cbcb0dc4a0e66b5ffba
kr521 <macao450@163.com>
2020-05-27 11:11:53 +0800

    修改Dockerfile编译方式

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,6 @@ RUN git clone $GIT_URL /$NAME && cd /$NAME && make
 FROM alpine:3.10
 
 ARG NAME="king-kubectl"
-ENV TIME_ZONE Asia/Shanghai
-RUN set -xe \
-    && sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
-    && apk --no-cache add tzdata curl \
-    && echo "${TIME_ZONE}" > /etc/timezone \
-    && ln -sf /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime \
-    && mkdir /lib64 \
-    && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
 COPY --from=builder /$NAME/entrypoint.sh /entrypoint.sh
 COPY --from=builder /$NAME/bin/$NAME /usr/local/bin
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+[ "$KUBECTL_URL" ] || https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl
+[ "$TIME_ZONE" ] || TIME_ZONE="Asia/Shanghai"
+[ "$ALPINE_REPO" ] || ALPINE_REPO="mirrors.aliyun.com"
+
+sed -i 's/dl-cdn.alpinelinux.org/${ALPINE_REPO}/g' /etc/apk/repositories     
+apk --no-cache add tzdata 
+echo "${TIME_ZONE}" > /etc/timezone 
+ln -sf /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime 
+mkdir /lib64 
+ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+ 
 # Get the kubectl binary.
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl
+curl -LO $KUBECTL_URL
 
 # Make the kubectl binary executable.
 chmod +x  ./kubectl
